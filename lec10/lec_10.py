1ef9ba10ffc6ae582b6408439e4de5798dcce6be
Hamad Asadullah <hamad.asadullah.nbt@gmail.com>
2025-11-29 10:32:45 -0800

    lec10 rem some code

--- lec10/lec_10.py
+++ lec10/lec_10.py
@@ -1,82 +1,71 @@
-# loops in python
-# fon loop - waile loop
-# print(1)
-# print(2)
-# print(3)
+loops in python
+fon loop - waile loop
+print(1)
+print(2)
+print(3)
 
 
-# # for loop
-# for i in "hamad":
-      #print("hello world", i)
+# for loop
+for i in "hamad":
+      print("hello world", i)
 
-# example for loop
-# for users in ["hamad" , "ali" , "hassan" , "banned user"]:
-#      if users == "banned user":
-#           print("permission denied")
-#      else:
-#           print("permission granted")
+example for loop
+for users in ["hamad" , "ali" , "hassan" , "banned user"]:
+     if users == "banned user":
+          print("permission denied")
+     else:
+          print("permission granted")
 
-# example 2  for loop by using break keyword
-# for money in [10 , 20 , 50 , 100 , 500 , 1000 , 5000]:
-#      if money == 1000:
-#           print("i have 1000rs")
-#           break
-#      else:
-#           print("i dont have 1000rs")
-
-
-
-
-# by using continue keyword
-# for money in [10 , 20 , 50 , 100 , 500 , 1000 , 5000]:
-#      if money == 1000:
-#           print("i have 1000rs")
-#           continue
-#      else:
-#           print("i dont have 1000rs")
-
-# by using pass keyword
-
-# for i in "hamad":
-#      pass # it will do nothing and continue the loop
-
-
-# lncrement operator  -  decreament
-# number = 1
-# number += 1
-# number += 1
-# number += 1
-# number -= 1
-# number -= 1
-
-# print(number)
+example 2  for loop by using break keyword
+for money in [10 , 20 , 50 , 100 , 500 , 1000 , 5000]:
+     if money == 1000:
+          print("i have 1000rs")
+          break
+     else:
+          print("i dont have 1000rs")
 
 
 
 
-# while loop
-# number = 1
-# while number <= 10:
-#      print("number is:", number)
+by using continue keyword
+for money in [10 , 20 , 50 , 100 , 500 , 1000 , 5000]:
+     if money == 1000:
+          print("i have 1000rs")
+          continue
+     else:
+          print("i dont have 1000rs")
+
+by using pass keyword
+
+for i in "hamad":
+     pass # it will do nothing and continue the loop
+
+
+lncrement operator  -  decreament
+number = 1
+number += 1
+number += 1
+number += 1
+number -= 1
+number -= 1
+
+print(number)
+
+
+
+
+while loop
+number = 1
+while number <= 10:
+     print("number is:", number)
+     number += 1
+
+example 1 while loop
+number = 1
+while number <= 10:
+     print("5 x", number , "=", number * 5)
 #      number += 1
 
-# example 1 while loop
-# number = 1
-# while number <= 10:
-#      print("5 x", number , "=", number * 5)
-# #      number += 1
+numbar guessing game using while loop
 
-# numbar guessing game using while loop
-
-secret_number = 7
-attempts = 0
-
-guess_number = int(input("guess the number between 1 to 10: "))
-if guess_number <= 0 or guess_number > 10:
-     print("please enter a valid number between 1 to 10")
-else:
-      while guess_number != secret_number
-             attempts += 1
-             print("wrong guess try again")
-                  guess_number = int(input("guess the number between 1 to 10: ")) 
      
